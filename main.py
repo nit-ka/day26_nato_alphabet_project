1253a6569c9dc06036f2133e0a476b7be14c2d0d
Kinga Nit <nit.kinga@gmail.com>
2022-07-07 11:58:02 +0200

    Initial commit

---
+++ main.py
@@ -0,0 +1,26 @@
+import pandas
+
+# # Looping through dictionaries:
+# for (key, value) in student_dict.items():
+#     #Access key and value
+#
+#
+# # Loop through rows of a data frame
+# for (index, row) in student_data_frame.iterrows():
+#     #Access index and row
+#     #Access row.student or row.score
+#
+# # Keyword Method with iterrows()
+# {new_key:new_value for (index, row) in df.iterrows()}
+
+nato_phonetic_alphabet = pandas.read_csv("nato_phonetic_alphabet.csv")
+phonetic_dict = {row.letter:row.code for (index, row) in nato_phonetic_alphabet.iterrows()}
+
+
+def change_into_phonetic_code_word(user_input):
+    code_words_list = [phonetic_dict[letter] for letter in user_input]
+    return code_words_list
+
+
+user_input = input("Enter a word: ").upper()
+print(change_into_phonetic_code_word(user_input))
