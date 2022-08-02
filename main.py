f6786107642b091d4b3f3e09eb7bf81e7c002195
Kinga Nit <nit.kinga@gmail.com>
2022-08-02 10:48:10 +0200

    Key error exception added

--- main.py
+++ main.py
@@ -14,13 +14,18 @@ import pandas
 # {new_key:new_value for (index, row) in df.iterrows()}
 
 nato_phonetic_alphabet = pandas.read_csv("nato_phonetic_alphabet.csv")
-phonetic_dict = {row.letter:row.code for (index, row) in nato_phonetic_alphabet.iterrows()}
+phonetic_dict = {row.letter: row.code for (index, row) in nato_phonetic_alphabet.iterrows()}
 
 
-def change_into_phonetic_code_word(user_input):
-    code_words_list = [phonetic_dict[letter] for letter in user_input]
-    return code_words_list
+def change_into_phonetic_code_word():
+    user_input = input("Enter a word: ").upper()
+    try:
+        code_words_list = [phonetic_dict[letter] for letter in user_input]
+    except KeyError:
+        print("Sorry, only letters in the alphabet please.")
+        change_into_phonetic_code_word()
+    else:
+        print(code_words_list)
 
 
-user_input = input("Enter a word: ").upper()
-print(change_into_phonetic_code_word(user_input))
+change_into_phonetic_code_word()
